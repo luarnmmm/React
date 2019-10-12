92c8b82645c0a390c9727df0e6e58360c3f9064c
PingPig <ping.luarn@go.shoreline.edu>
2019-10-12 12:11:10 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "huscii-notes"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: ""
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_L10bHPFWW",
+        APP_CLIENT_ID: "2socgj3ch9j9mdnav23nqasi0f",
+        IDENTITY_POOL_ID: ""
+    }
+};
